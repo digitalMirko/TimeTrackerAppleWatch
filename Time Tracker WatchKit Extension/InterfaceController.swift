02ad2f65cc674aee2cbcc14a6714a1f3b3ffdd43
Mirko Cukich <mirkocukich@gmail.com>
2016-11-13 22:45:33 -0600

    Adding Outlets and Actions

--- Time Tracker WatchKit Extension/InterfaceController.swift
+++ Time Tracker WatchKit Extension/InterfaceController.swift
@@ -12,11 +12,16 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+    
+    @IBOutlet var topLbl: WKInterfaceLabel!
+    @IBOutlet var middleLbl: WKInterfaceLabel!
+    @IBOutlet var clockBtn: WKInterfaceButton!
+    
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
         
         // Configure interface objects here.
-         
+        
     }
     
     override func willActivate() {
@@ -27,6 +32,10 @@ class InterfaceController: WKInterfaceController {
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
+    }
+    
+    @IBAction func clockBtnAction() {
+        clockBtn.setTitle("Clock-Out")
     }
 
 }
