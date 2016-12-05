3dbf6d3011f1300609a40eabfc91ea87928a06c2
Mirko Cukich <mirkocukich@gmail.com>
2016-12-04 23:49:52 -0600

    Date Formatting

--- Time Tracker WatchKit Extension/DetailInterfaceController.swift
+++ Time Tracker WatchKit Extension/DetailInterfaceController.swift
@@ -28,8 +28,11 @@ class DetailInterfaceController: WKInterfaceController {
         let clockIn = dates[0]
         let clockOut = dates[1]
             
-        clockInLabel.setText(clockIn.description)
-        clockOutLabel.setText(clockOut.description)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "MMM d h:mm:ssa"
+            
+            clockInLabel.setText(formatter.string(from: clockIn))
+            clockOutLabel.setText(formatter.string(from: clockOut))
             
             
         }
